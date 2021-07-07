32db0b68a99546678872a3e7376ead7bea508c43
Mo <None>
2021-07-07 03:14:59 -0700

    about component created, menu component started

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,37 +3,31 @@ import React, { Component } from 'react';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
-
+import {items} from "../shared/items"
 import About from './AboutComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import Menu from './MenuComponent';
 
+class Main extends Component {
+constructor(props) {
+  super(props)
 
-class Main extends Component{
-
-
-render(){
-
-
-
-
-
-
-
-return(
-
-<div>
-<Header></Header>
-<Footer></Footer>
-
-</div>
-
-
-)
-
-
-
+  this.state = {
+     items:items
+  }
 }
 
 
+    render() {
+        return (
+            <div>
+                <Header />
+                <div>{this.state.items.map(item=>item.name)} </div>
+                <About />
+                <Menu items={this.state.items} />
+                <Footer />
+            </div>
+        );
+    }
 }
-export default Main
+export default Main;
--- src/shared/items.js
+++ src/shared/items.js
@@ -1,4 +1,4 @@
-export const products = [
+export const items = [
     {
         id: 0,
 
